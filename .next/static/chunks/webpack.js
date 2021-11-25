2aaa9b99cc3973fe2d4fa826ab669de3f4b507d2
FIRST_NAME LAST_NAME <jannickpedersen@live.dk>
2021-11-25 11:43:23 +0100

    sanity client and get blogs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75173d43e520304a"; }
+/******/ 		__webpack_require__.h = function() { return "9edcf24e4fd7b663"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

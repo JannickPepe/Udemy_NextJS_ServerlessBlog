abdd6333a3420164dd7eb1e94b1b6c13496ba5ae
FIRST_NAME LAST_NAME <jannickpedersen@live.dk>
2021-12-08 11:10:01 +0100

    useSWR

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 
 import { useState } from 'react';
+import useSWR from 'swr';
 import { Row, Col } from 'react-bootstrap';
 import PageLayout from '../components/PageLayout';
 import AuthorIntro from '../components/AuthorIntro';
@@ -9,11 +10,18 @@ import FilteringMenu from '../components/FilteringMenu';
 
 import { getAllBlogs } from '../lib/api';
 
+// we create the fetcher function so the SWR function can be more usable
+const fetcher = url => fetch(url).then(res => res.json())
+
 // Iterate over the blogs and map the blog
 export default function Home({blogs}) {
   const [filter, setFilter] = useState({
     view: { list: 0 }
   });
+
+  // create the useSWR function with the path string and fether function
+  const { data, error } = useSWR('../api/hello', fetcher);
+  debugger
 
   return (
     <PageLayout>
@@ -33,7 +41,16 @@ export default function Home({blogs}) {
         { blogs.map(blog =>
           filter.view.list ?
             <Col key={`${blog.slug}-list`} md="9">
-              <CardListItem />
+            <CardListItem
+                author={blog.author}
+                title={blog.title}
+                subtitle={blog.subtitle}
+                date={blog.date}
+                link={{
+                  href: '/blogs/[slug]',
+                  as: `/blogs/${blog.slug}`
+                }}
+              />
             </Col>
             :
             <Col key={blog.slug} md="4">
